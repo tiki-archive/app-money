fbd78c0e30a41ba023bff4633a50862449bd8d74
Ricardo Gonçalves <ricardo@mytiki.com>
2022-02-24 12:30:59 -0300

    init

---
+++ example/android/app/src/main/kotlin/com/mytiki/money_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mytiki.money_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Classes/MoneyPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface MoneyPlugin : NSObject<FlutterPlugin>
+@end
