e40b67f743932ece651127589737c659e3f3f60d
Ricardo Gonçalves <ricardo@mytiki.com>
2022-02-25 11:54:46 -0300

    money header

--- lib/src/money_style.dart
+++ lib/src/money_style.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/material.dart';
+
 import 'utils/money_utils_style.dart';
 
 class MoneyStyle extends MoneyUtilsStyle {
-  MoneyStyle();
+  final Color _headerColor;
+  final String _headerFontFamily;
 
-  get headerColor => null;
+  MoneyStyle({
+    headerColor = const Color(0xFF00133F),
+    headerFontFamily = "Arial"
+  }) :
+   _headerColor = headerColor,
+   _headerFontFamily = headerFontFamily;
 
-  get headerFontFamily => null;
+  get headerColor => _headerColor;
+
+  get headerFontFamily => _headerFontFamily;
 }
--- lib/src/ui/money_home_view_header.dart
+++ lib/src/ui/money_home_view_header.dart
@@ -6,10 +6,10 @@ class MoneyHomeViewHeader extends StatelessWidget{
 
   final String _title = "Data NFTs are on the way!";
   final String _text = '''
-  We have a few technical i’s to dot and t’s 
-  to cross but the core tech is in your app. 
-  Keep your 👀 peeled, your data NFTs will 
-  start showing up in a couple weeks.
+We have a few technical i’s to dot and t’s 
+to cross but the core tech is in your app. 
+Keep your 👀 peeled, your data NFTs will 
+start showing up in a couple weeks.
 ''';
 
   const MoneyHomeViewHeader({Key? key}) : super(key: key);
@@ -19,36 +19,35 @@ class MoneyHomeViewHeader extends StatelessWidget{
     MoneyService service = Provider.of<MoneyService>(context);
     return SizedBox(
       child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Text(_title, style: TextStyle(
-                  fontFamily: service.style.headerFontFamily,
-                  color: service.style.headerColor,
-                  fontSize: service.style.text(18),
-                  fontWeight: FontWeight.bold
-                ),),
-                Text(_text, style: TextStyle(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [ Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(_title,
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
                     fontFamily: service.style.headerFontFamily,
                     color: service.style.headerColor,
-                    fontSize: service.style.text(13),
-                ),)
-              ],
-            ),
-          ),
-          SizedBox(
-            width: service.style.size(70),
-            height: service.style.size(100),
-            child: Align(
-              alignment: Alignment.center,
-              child: Image.asset(
-                "pineapple-pizza",
+                    fontSize: service.style.text(18),
+                    fontWeight: FontWeight.bold
+                  ),),
+                  Padding(padding: EdgeInsets.only(bottom: service.style.size(10)),),
+                  Text(_text,
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                      fontFamily: service.style.headerFontFamily,
+                      color: service.style.headerColor,
+                      fontSize: service.style.text(13),
+                  ),)
+                ],
+              ),
+          Image.asset(
+                "res/images/pineapple-pizza.png",
                 package: "money",
-                width: service.style.size(68),
-                height: service.style.size(98),
-              )
-            )
+            alignment: Alignment.center,
           )
         ],
       ),
