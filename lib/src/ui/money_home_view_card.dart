cf8b947184a74b207d0624eb4f8f52349ece1ee0
Ricardo Gonçalves <ricardo@mytiki.com>
2022-04-29 09:26:34 -0300

    publish to pub

--- lib/src/ui/money_home_view_card.dart
+++ lib/src/ui/money_home_view_card.dart
@@ -10,7 +10,7 @@ class MoneyHomeViewCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: MediaQuery.of(context).size.width,
       height: SizeProvider.instance.height(215),
       child: FittedBox(
